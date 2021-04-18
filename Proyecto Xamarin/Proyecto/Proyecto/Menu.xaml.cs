06cf41893a9018d4b690bc686e455be6e6e12d68
JeanAmaya21 <62520037+JeanAmaya21@users.noreply.github.com>
2021-04-18 02:09:08 -0600

    botones

--- Proyecto Xamarin/Proyecto/Proyecto/Menu.xaml.cs
+++ Proyecto Xamarin/Proyecto/Proyecto/Menu.xaml.cs
@@ -16,6 +16,36 @@ namespace Proyecto
         {
             InitializeComponent();
             BtnModelosToyota.Clicked += BtnModelosToyota_Clicked;
+            BtnModelosFord.Clicked += BtnModelosFord_Clicked;
+            BtnModelosHonda.Clicked += BtnModelosHonda_Clicked;
+            BtnModelosLexus.Clicked += BtnModelosLexus_Clicked;
+            BtnModelosNissan.Clicked += BtnModelosNissan_Clicked;
+            BtnModelosKia.Clicked += BtnModelosKia_Clicked;
+        }
+
+        private void BtnModelosKia_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnModelosNissan_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnModelosLexus_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnModelosHonda_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnModelosFord_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void BtnModelosToyota_Clicked(object sender, EventArgs e)
