dd337a30e5121c093b2455639473c60cc86338d1
sebasq10 <69024432+sebasq10@users.noreply.github.com>
2021-04-19 19:23:45 -0600

    Update Menu.xaml.cs

--- Proyecto Xamarin/Proyecto/Proyecto/Menu.xaml.cs
+++ Proyecto Xamarin/Proyecto/Proyecto/Menu.xaml.cs
@@ -30,65 +30,62 @@ namespace Proyecto
 
         private void BtnModelosKia_Clicked(object sender, EventArgs e)
         {
-            /*
             marca = "kia";
-            URLmodelo1 = "";
-            URLmodelo2 = "";
-            URLmodelo3 = "";
+            URLmodelo1 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833864740571840542/StringerBlack.png");//18:40 carros negros 1er modelo
+            URLmodelo2 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833864749250510918/NiroEVRed.png"); //18:40 carro rojo 2ndo modelo
+            URLmodelo3 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833864756825554994/RioWhite.png");//18:40 carro blanco 3er modelo
 
-            */
+            ((NavigationPage)this.Parent).PushAsync(new MenuModelos(URLmodelo1, URLmodelo2, URLmodelo3));
         }
 
         private void BtnModelosNissan_Clicked(object sender, EventArgs e)
         {
             /*
             marca = "nissan";
-            URLmodelo1 = "";
-            URLmodelo2 = "";
-            URLmodelo3 = "";
+            URLmodelo1 = new Uri ("");
+            URLmodelo2 = new Uri ("");
+            URLmodelo3 = new Uri ("");
 
+            ((NavigationPage)this.Parent).PushAsync(new MenuModelos(URLmodelo1, URLmodelo2, URLmodelo3));
             */
         }
 
         private void BtnModelosLexus_Clicked(object sender, EventArgs e)
         {
-            /*
             marca = "lexus";
-            URLmodelo1 = "";
-            URLmodelo2 = "";
-            URLmodelo3 = "";
+            URLmodelo1 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833869665897021450/UXHybridBlack.png");//19:00 carro negro 1er modelo (enviado al final)
+            URLmodelo2 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833869658737344562/LCConvertibleRed.png");//19:00 carro rojo 2ndo modelo
+            URLmodelo3 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833869663456722954/RCFWhite.png");//19:00carro blanco 3er modelo
 
-            */
+            ((NavigationPage)this.Parent).PushAsync(new MenuModelos(URLmodelo1, URLmodelo2, URLmodelo3));
         }
 
         private void BtnModelosHonda_Clicked(object sender, EventArgs e)
         {
-            /*
             marca = "honda";
-            URLmodelo1 = "";
-            URLmodelo2 = "";
-            URLmodelo3 = "";
+            URLmodelo1 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859428029300796/CRVCrystalBlackPearl.png");//18:19 carro negro 1er modelo
+            URLmodelo2 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859408425254932/HR-VAegeanBlue.png");//18:19 carro azul 2ndo modelo
+            URLmodelo3 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859425411268628/AccordPlatinumWhite.png");//18:19 carro blanco 3er modelo
 
-            */
+            ((NavigationPage)this.Parent).PushAsync(new MenuModelos(URLmodelo1, URLmodelo2, URLmodelo3));
         }
 
         private void BtnModelosFord_Clicked(object sender, EventArgs e)
         {
-            /*
             marca = "ford";
-            URLmodelo1 = "";
-            URLmodelo2 = "";
-            URLmodelo3 = "";
+            URLmodelo1 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859793545855016/Oxford_White.png");//18:21 carro blanco 1er modelo
+            URLmodelo2 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859799090462801/EcoAbsoluteBlackEco.png");//18:21 carro negro 2ndo modelo
+            URLmodelo3 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859807055708170/ExplorerRapidRed.png");//18:21 carro rojo 3er modelo
 
-            */
+            ((NavigationPage)this.Parent).PushAsync(new MenuModelos(URLmodelo1, URLmodelo2, URLmodelo3));
         }
 
         private void BtnModelosToyota_Clicked(object sender, EventArgs e)
         {
             marca = "toyota";
-            URLmodelo1 = new Uri ("https://cdn.discordapp.com/attachments/730938576560193607/833841319260586075/AGYANaranja.png");
-            URLmodelo2 = new Uri ("https://cdn.discordapp.com/attachments/730938576560193607/833841315850354738/YarisRedMicaMetallic.png");
-            URLmodelo3 = new Uri ("https://cdn.discordapp.com/attachments/730938576560193607/833841327837806602/LandCruiserPradoGrayMetallic.jpg");
+            URLmodelo1 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859172847714334/LandCruiserPradoBlack.jpg");//18:18 carro negro 1er modelo
+            URLmodelo2 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859184381526026/YarisRedMicaMetallic.png");//18:18 carro rojo 2ndo modelo
+            URLmodelo3 = new Uri ("https://media.discordapp.net/attachments/730938576560193607/833859187909066782/AGYASilverMetallic.png");//18:18 carro plateado 3er modelo
 
             ((NavigationPage)this.Parent).PushAsync(new MenuModelos(URLmodelo1, URLmodelo2, URLmodelo3));
         }
